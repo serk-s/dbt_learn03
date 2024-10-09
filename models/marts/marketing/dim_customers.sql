77ffaf14ee93c6ae7db0ff63af02d22086a95f8a
serkansuner987+dbt03@gmail.com <serkansuner987+dbt03@gmail.com>
2024-10-09 12:04:39 +0000

    bla bla

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -1,18 +1,12 @@
-{{
-  config(
-    materialized='view'
-  )
-}}
-
 with customers as (
 
-    select * from {{ ref('stg_customers') }}
+    select * from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
 orders as (
 
-    select * from {{ ref('stg_orders') }}
+    select * from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
